36000ea0839ffa97d2777ac1dc1422cc545f111d
Feather <spacey2001@gmail.com>
2014-05-14 10:47:52 +0200

    Final commit :)

---
+++ Project Euler 15/Problem15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Problem16
+{
+   public class Program
+   {
+      // Problem 16
+      // Power Digit Sum
+      // 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+      // What is the sum of the digits of the number 2^1000?
+      static void Main(string[] args)
+      {
+
+      }
+
+      public static double GetPowerDigitSum(int number, int power)
+      {
+         // This is a fairly easy problem, let's solve this with one line of code.
+         return  BigInteger.Pow(number, power)
+                .ToString()
+                .ToCharArray()
+                .Where(entry => char.IsNumber(entry) && char.GetNumericValue(entry) != 0)
+                .Select(entry => char.GetNumericValue(entry))
+                .Sum();
+      }
+   }
+}
---
+++ Project Euler 15/Problem15Test/Problem16Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem16;
+
+namespace Problem16Test
+{
+   [TestClass]
+   public class Problem16Test
+   {
+      [TestMethod]
+      public void TestPowerDigitSumExample()
+      {
+         //Arrange
+         int number = 2;
+         int power = 15;
+         double expected = 26.0;
+         //Act
+         double actual = Program.GetPowerDigitSum(number, power);
+         //Assert
+         Assert.AreEqual(expected, actual);
+      }
+
+      [TestMethod]
+      public void TestPowerDigitSumProblem()
+      {
+         //Arrange
+         int number = 2;
+         int power = 1000;
+         double expected = 1366;
+         //Act
+         double actual = Program.GetPowerDigitSum(number, power);
+         //Assert
+         Assert.AreEqual(expected, actual);
+      }
+   }
+}

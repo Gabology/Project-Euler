73d4c4afd5dc1f556dd44e8abb99b8fb72c61ea9
Feather <spacey2001@gmail.com>
2014-05-21 11:23:47 +0200

    1LOC solution ;)

---
+++ ProjectEuler19/ProjectEuler19/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler19
+{
+   class Program
+   {
+      // Problem 19
+      // Counting Sundays
+      // You are given the following information, but you may prefer to do some research for yourself.
+      //    1 Jan 1900 was a Monday.
+      //    Thirty days has September,
+      //    April, June and November.
+      //    All the rest have thirty-one,
+      //    Saving February alone,
+      //    Which has twenty-eight, rain or shine.
+      //    And on leap years, twenty-nine.
+      //    A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+      // How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+      static void Main(string[] args)
+      {
+         Console.Write(
+            Enumerable.Range(0, 1200)
+            .Select(months => new DateTime(1901, 01, 01).AddMonths(months))
+            .Count(date => date.DayOfWeek == DayOfWeek.Sunday)
+            );
+         Console.ReadKey();
+      }
+   }
+}
